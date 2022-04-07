10d5cd1aa6c3de51305fea81e5b4bdb36d11b8c8
Katie Volz <iggyvolz@gmail.com>
2022-04-07 17:33:07 +0000

    Add logging to HTTP server, respond with HTTP/1.1 in fallback case

--- src/HttpServer.php
+++ src/HttpServer.php
@@ -6,6 +6,8 @@ use Nyholm\Psr7\ServerRequest;
 use Psl\TCP\Server;
 use Psl\TCP\ServerOptions;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use function Psl\Async\run;
 use function Psl\Str\format;
 
@@ -16,6 +18,7 @@ class HttpServer extends BaseHttp
         public readonly string $host = "0.0.0.0",
         public readonly int $port = 80,
         public readonly ?ServerOptions $serverOptions = null,
+        private readonly LoggerInterface $logger = new NullLogger(),
     )
     {
     }
@@ -34,13 +37,15 @@ class HttpServer extends BaseHttp
                     $response = $this->requestHandler->handle($serverRequest);
                     $connection->writeAll(format("HTTP/%s %d %s\r\n", $response->getProtocolVersion(), $response->getStatusCode(), $response->getReasonPhrase()));
                     self::writeMessage($response, $connection);
+                    $this->logger->info("$method $path " . $response->getStatusCode());
                     if($response instanceof AfterMessageHandler) {
                         $response->handle($connection);
                     } else {
                         $connection->close();
                     }
-                } catch (\Throwable) {
-                    $connection->write("HTTP/0.9 500 Internal Server Error\r\n\r\n");
+                } catch (\Throwable $e) {
+                    $this->logger->error("$method $path 500: $e");
+                    $connection->write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
                     $connection->close();
                 }
             });
