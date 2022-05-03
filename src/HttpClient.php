7ed29e7c8fb6aff247637081cfa6b148f9d1771d
Katie Volz <iggyvolz@gmail.com>
2022-05-03 19:38:23 +0000

    Update for https

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -14,7 +14,8 @@ class HttpClient extends BaseHttp implements ClientInterface
 
     public function sendRequest(RequestInterface $request): ResponseInterface
     {
-        $connection = connect($request->getUri()->getHost(), $request->getUri()->getPort() ?? 80);
+        $https = $request->getUri()->getScheme() === "https";
+        $connection = connect($request->getUri()->getHost(), $request->getUri()->getPort() ?? ($https ? 443 : 80), tls: $https);
         $connection->writeAll(format("%s %s HTTP/%s\r\n", $request->getMethod(), $request->getUri()->getPath() . (($request->getUri()->getQuery() === "" ? "" : "?" . $request->getUri()->getQuery())), $request->getProtocolVersion()));
         $this->writeMessage($request, $connection);
         $version = substr($this->readWord($connection), 5);
